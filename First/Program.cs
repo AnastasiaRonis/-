4a8a714dfa283621f6efa130a2a0b803f89094f5
Anastasia <anastasiauneauretour@yandex.ru>
2022-09-01 22:18:30 +0900

    Вторая задача

---
+++ First/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a>b)
+{
+    Console.WriteLine("a является большим числом");
+}
+else if(b>a)
+{
+    Console.WriteLine("a является меньшим числом");
+}
---
+++ Second/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if(a>max) 
+{
+    max=a;
+}
+else if(b>max) 
+{
+    max=b;
+}
+else if(c>max) 
+{
+    max=c;
+}
+
+Console.WriteLine(max);
