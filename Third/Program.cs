f065b86c2e18cf29a7f3b82e48d166ddef26ba62
Anastasia <anastasiauneauretour@yandex.ru>
2022-09-01 22:41:55 +0900

    Третья задача

---
+++ Third/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number%2==0)
+{
+    Console.WriteLine("number делится на 2");
+}
+else
+{
+    Console.WriteLine("number не делится на 2");
+}
+Console.WriteLine(number);
